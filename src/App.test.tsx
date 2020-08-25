4d473db16475d77ceec10fcfc4bbc3ba3d90e025
Ilya Ivanov <static.ila@gmail.com>
2020-08-25 09:45:37 +0300

    Expand/collapse with a triangle

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const app = () => render(<App />)
+
+it('Should show Root 1 node byu default"', () => {
+  expect(app().getByText("Root 1")).toBeInTheDocument();
+});
+
+it('should not show Sub 1.1 node (first child of collapsed Root 1 node)', function () {
+  expect(app().queryByText("Sub 1.1")).toBeNull();
+});
+
+
+it('when clicking on a triangle for a Root 1 node its children should be visible', function () {
+  const { queryByText, getByTestId } = app();
+  fireEvent.click(getByTestId('triangle-1'))
+  expect(queryByText("Sub 1.1")).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const id = () => Math.random() + "";
+
+const item = (title: string, children: Item[] = []): Item => ({
+  id: title.split(" ").pop() || id(),
+  title,
+  children,
+  isOpen: false,
+});
+
+interface Item {
+  id: string;
+  title: string;
+  children: Item[];
+  isOpen: boolean;
 }
 
+class App extends React.Component {
+  state = {
+    items: [
+      item("Root 1", [
+        item("Sub 1.1", [item("Sub 1.1.1"), item("Sub 1.1.2")]),
+        item("Sub 1.2"),
+      ]),
+      item("Root 2"),
+      item("Root 3"),
+    ],
+  };
+
+  toggleOpenClosed = (itemToToggle: Item) => {
+    const newItems = this.state.items.map((item) =>
+      this.toggleItem(item, itemToToggle.id)
+    );
+    this.setState({ items: newItems });
+  };
+
+  toggleItem = (item: Item, id: string): Item => {
+    if (item.id === id)
+      return {
+        ...item,
+        isOpen: !item.isOpen,
+      };
+    else
+      return {
+        ...item,
+        children: item.children.map((i) => this.toggleItem(i, id)),
+      };
+  };
+
+  renderItem = (item: Item, level: number): JSX.Element => (
+    <Fragment key={item.id}>
+      <ItemView onPress={this.toggleOpenClosed} item={item} level={level} />
+      {item.isOpen &&
+        item.children.map((item) => this.renderItem(item, level + 1))}
+    </Fragment>
+  );
+
+  render() {
+    return (
+      <div className="page">
+        {this.state.items.map((item) => this.renderItem(item, 0))}
+      </div>
+    );
+  }
+}
 export default App;
+
+//UI primitives for the app
+
+const ItemView = ({
+  item,
+  onPress,
+  level = 0,
+}: {
+  item: Item;
+  onPress: (item: Item) => void;
+  level?: number;
+}) => (
+  <div className="row" style={{ paddingLeft: level * 20 }}>
+    <Triangle
+      className={
+        "triangle " +
+        (item.children.length === 0 ? "transparent " : "") +
+        (item.isOpen ? "open " : "")
+      }
+      data-testid={"triangle-" + item.id}
+      onClick={() => onPress(item)}
+    />
+    <div className={`circle`} />
+    <div className="text">{item.title}</div>
+  </div>
+);
+
+const Triangle = (props: any) => {
+  const width = 20;
+  const verticalPadding = 7;
+  const horizontalPadding = 4;
+  return (
+    <svg
+      height={width}
+      width={width}
+      viewBox={`0 0 ${width} ${width}`}
+      {...props}
+    >
+      <polygon
+        points={`
+        ${verticalPadding},${horizontalPadding} 
+        ${width - verticalPadding},${width / 2} 
+        ${verticalPadding},${width - horizontalPadding}`}
+      />
+    </svg>
+  );
+};
