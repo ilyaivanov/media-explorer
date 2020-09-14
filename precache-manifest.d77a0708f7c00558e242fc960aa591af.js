e98179eb5879c7dacfaee005259e37c53b135f54
Ilya Ivanov <static.ila@gmail.com>
2020-09-14 10:11:43 +0300

    Updates

---
+++ precache-manifest.d77a0708f7c00558e242fc960aa591af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a115429d90c4a152f6dadd46e43c3b10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "831840dfdb65e57b79f8",
+    "url": "/static/css/main.820de808.chunk.css"
+  },
+  {
+    "revision": "43568e0b46da26f3f379",
+    "url": "/static/js/2.c0397a16.chunk.js"
+  },
+  {
+    "revision": "32a1db9b530bdb72069398e9021c45c2",
+    "url": "/static/js/2.c0397a16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831840dfdb65e57b79f8",
+    "url": "/static/js/main.5219777d.chunk.js"
+  },
+  {
+    "revision": "23daa37aa1639d589c4a",
+    "url": "/static/js/runtime-main.fdc00350.js"
+  }
+]);
